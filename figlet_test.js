89b923dd4519714afc8f2cf7933e9d0281515909
Veldas R Durai <veldasrdurai72@gmail.com>
2022-01-30 01:10:56 +0530

    new packages added

---
+++ figlet_test.js
@@ -0,0 +1,14 @@
+import figlet from 'figlet';
+
+// visit : https://www.npmjs.com/package/figlet
+
+figlet("Hello World...!", {
+        font: 'epic',
+            // For different font types : http://www.figlet.org/examples.html
+        horizontalLayout: 'fitted', // "default", "full", "fitted", "controlled smushing", "universal smushing".
+        verticalLayout: 'fitted',
+        // width: 80,
+        whitespaceBreak: true
+    }, ( err, data ) => {
+        console.log( data );
+})
